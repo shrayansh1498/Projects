1ab66ed3788a2c12e1cee8626339b35923e60e58
Shrayansh <shrayansh8991@gmail.com>
2026-04-18 14:26:59 +0530

    some change

--- LLD1/temp.java
+++ LLD1/temp.java
@@ -1,5 +1,13 @@
 package LLD1;
 
+import java.util.Scanner;
+
 public class temp {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an integer");
+        int number = sc.nextInt();
+        System.out.println("You entered: " + number);
+        sc.close();
+    }
 }
