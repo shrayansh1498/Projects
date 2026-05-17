e6ff040c7659a419f853b13a66790f0f96afe5b6
Shrayansh <shrayansh8991@gmail.com>
2026-05-17 14:13:13 +0530

    temp1 change

--- LLD1/AryanM/Creational Design Pattern/FactoryDesignPattern/FactoryDesignPatternBefore.java
+++ LLD1/AryanM/Creational Design Pattern/FactoryDesignPattern/FactoryDesignPatternBefore.java
@@ -7,5 +7,21 @@ public class FactoryDesignPatternBefore {
     public static void main(String[] args)
     {
         System.out.println("Creating Car object");
+        String vehicleType = "Truck";
+        if (vehicleType.equals("Car")) 
+        {
+            System.out.println("Car");
+        } 
+        else if (vehicleType.equals("Truck")) 
+        {
+            System.out.println("Truck");        } 
+        else if (vehicleType.equals("Bike")) 
+        {
+            System.out.println("Bike");
+        } 
+        else 
+        {
+        throw new IllegalArgumentException("Unknown vehicle type");
+        }
     }
 }
